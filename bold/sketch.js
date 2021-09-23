de039140e1849d89d29bfac048f69902f0d262d7
Senaid <senaidcivic5@gmail.com>
2021-09-23 14:49:53 +0200

    shat

--- bold/sketch.js
+++ bold/sketch.js
@@ -2,7 +2,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+let xPos=windowWidth/2
+let yPos=windowHeight/2
+let Diameter=100
+
 function draw() {
   background(255);
-
+circle(xPos,yPos,Diameter);
 }
